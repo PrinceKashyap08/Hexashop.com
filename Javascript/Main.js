3dbb359945cb2a79e052277c5d9458d0152af727
Prince <vasukashyap08@gmail.com>
2024-02-13 16:35:58 +0530

    commit

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,2 +1,13 @@
-const year = document.querySelector("#currentYear")
-year.innerHTML = new Date().getFullYear();
+// const year = document.querySelector("#currentYear")
+// year.innerHTML = new Date().getFullYear();
+
+// hamburger 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
