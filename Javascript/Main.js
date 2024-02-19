30b766a933277711accdc7468da9380c79874a28
Prince <vasukashyap08@gmail.com>
2024-02-19 16:16:48 +0530

    commit

--- Javascript/Main.js
+++ Javascript/Main.js
@@ -1,9 +1,14 @@
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+    header.classList("sticky",window.scrollY > 0);
+});
 // Pre Loader  
 const preloader = document.getElementById("loader");
 
 function myLoader(){
     preloader.style.display = "none";
 };
+
 
 // CurrentYear 
 const year = document.querySelector("#currentYear")
@@ -21,7 +26,45 @@ function mobileMenu() {
 };
 
 // menLatest 
-$('#menLatest .owl-carousel').owlCarousel({
+$('#mens .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    autoplayTimeout:5000,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+$('#womens .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    autoplayTimeout:5000,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+$('#kids .owl-carousel').owlCarousel({
     loop: true,
     margin: 30,
     nav: false,
